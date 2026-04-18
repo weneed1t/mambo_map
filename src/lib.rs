a687a58ae09b75e44b73e56e4004da4655291f2c
weneed1t <sladost.nikituncik@gmail.com>
2026-04-18 18:07:13 +0000

    idk 0.1.31

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,4 @@ cargo clippy --fix --all --allow-dirty
 dotnet run -- all check-non-fix potential
 */
 
- //git add . && git commit -S -m "Improved code security and formatting, added new tests and fmt settings 0.1.30" && cargo publish
+//git add . && git commit -S -m "Improved code security and formatting, added new tests and fmt settings 0.1.30" && cargo publish && git push
